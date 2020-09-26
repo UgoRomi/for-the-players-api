a37f62e6d42a1015ae3fabd35b0ffb83d2476a19
Ugo Romi <ugo.romi@gmail.com>
2020-09-26 13:20:27 +0200

    Added initial path to check heroku config

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const helmet = require('helmet')
 
 const app = express()
+app.use(helmet())
+app.use(morgan('short'))
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'working'
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
+})
