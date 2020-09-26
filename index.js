6546e33638c3133688a97b13a34ba5630f16360c
Ugo Romi <ugo.romi@gmail.com>
2020-09-26 15:29:03 +0200

    Added prettier and more auth routes

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-const app = express()
-app.use(helmet())
-app.use(morgan('short'))
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'working'
-    })
-})
+// Middlewares
+app.use(helmet());
+app.use(morgan("short"));
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require("./routes/auth");
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "working",
+  });
+});
+
+app.get("/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+  console.log(`listening on port ${process.env.PORT}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 /**
  * @api {post} /auth/register Register a new user
@@ -6,12 +6,39 @@ const router = require('express').Router()
  * @apiGroup Auth
  *
  * @apiParam {String} username          The new user's username
- * @apiParam {String} Email             The new user's email
- * @apiParam {String} Password          The new user's password
+ * @apiParam {String} email             The new user's email
+ * @apiParam {String} password          The new user's password
  *
  * @apiSuccess {String} username        Username of the newly registered user
  * @apiSuccess {String} email           Email of the newly registered user
  */
-router.post('register', (req, res) => {
-    console.log('register called')
-})
+router.post("register", (req, res) => {
+  res.json({ message: "register called" });
+});
+
+/**
+ * @api {post} /auth/login Login
+ * @apiName Login
+ * @apiGroup Auth
+ *
+ * @apiParam {String} email             The user's email
+ * @apiParam {String} password          The user's password
+ *
+ * @apiSuccess {String} jwt             JWT for the session
+ */
+router.post("login", (req, res) => {
+  res.json({ message: "login called" });
+});
+
+/**
+ * @api {post} /auth/forgot-password Forgot Password
+ * @apiName Forgot Password
+ * @apiGroup Auth
+ *
+ * @apiParam {String} email             The user's email
+ */
+router.post("forgot-password", (req, res) => {
+  res.json({ message: "forgot-password called" });
+});
+
+module.exports = router;
